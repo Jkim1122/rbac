8a7feea224e1f5d75e7ff20825330acae3d0c520
ADTanner <sgtadt@gmail.com>
2024-08-14 11:38:18 -0500

    added simple django auth implementation with home page, sign up, log in, and log out functionality.

---
+++ accounts/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+#Home View
+def home(request):
+    return render(request, 'home.html')
+
+#Signup View:
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
+
+#Login View:
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+            
+#Logout View:
+def logout_view(request):
+    logout(request)
+    return redirect('login')
