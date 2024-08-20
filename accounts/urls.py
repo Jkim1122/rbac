60bfbee344476d844014115bb670900157571241
ADTanner <sgtadt@gmail.com>
2024-08-20 11:00:06 -0500

    added password change functionality to profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
@@ -10,4 +11,11 @@ urlpatterns=[
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('logout/success/', views.logout_success, name='logout_success'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html'
+    ), name='password_change'),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name='password_change_done'),
 ]
